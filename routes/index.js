38c9e76f06ce13e9f7a0fbf169156ce9782b8467
tayutaedomo <tayutaedomo@gmail.com>
2017-07-12 22:10:43 +0900

    Add spike/checkout

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.get('/spike/embed', function(req, res, next) {
   res.render('spike/embed', { title: 'SPIKE Embed' });
 });
 
+router.get('/spike/checkout', function(req, res, next) {
+  res.render('spike/checkout', { title: 'SPIKE Checkout' });
+});
+
 
 module.exports = router;
 
